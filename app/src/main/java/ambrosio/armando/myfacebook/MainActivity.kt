937d87431a04fdee00a6614876d2d57e525ecd3f
ArmandoAmbrosioS <armando.ambrosiosoto@gmail.com>
2022-02-15 23:49:43 -0700

    First commit

---
+++ app/src/main/java/ambrosio/armando/myfacebook/MainActivity.kt
@@ -0,0 +1,11 @@
+package ambrosio.armando.myfacebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
